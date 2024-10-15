7090fac807c35775a8a3f3cb301585f26a6cf178
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2024-10-15 14:32:32 +0200

    extend config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import {StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'rsuite/dist/rsuite.min.css';
+import {RootComponent} from "./RootComponent.tsx";
+
+
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RootComponent>
+      <App />
+    </RootComponent>
+  </StrictMode>
 )
+
