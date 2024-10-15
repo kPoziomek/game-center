8c6ba916863b252936f9a3f394ec25a6932c94fa
Krzysztof Poziomek <krzysztof.poziomek@gmail.com>
2024-10-15 14:32:32 +0200

    extend config

---
+++ src/RootComponent.tsx
@@ -0,0 +1,16 @@
+import {ReactNode, useState} from "react";
+import {CustomProvider} from "rsuite";
+
+type RootComponentProps = {
+  children: ReactNode
+}
+
+export const RootComponent = ({children}:RootComponentProps) => {
+  const [theme,] = useState<"dark" | "light" | "high-contrast" | undefined>('dark');
+
+  return( <CustomProvider theme={theme}>
+      {children}
+    </CustomProvider>
+  )
+};
+
